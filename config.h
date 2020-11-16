a5a475386fc6b6fff490ac323b8346e5f55f6476
MrMip <Miphamster@protonmail.com>
2020-11-15 21:56:38 -0600

    switched to st

--- config.h
+++ config.h
@@ -57,7 +57,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", dracula_bg, "-nf", dracula_fg, "-sb", dracula_purp, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
+//static const char *termcmd[]  = { "alacritty", NULL };
 static const char *textcmd[]  = { "emacs", NULL };
 
 static Key keys[] = {
