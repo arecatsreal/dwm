bab53471d107b64b5c7a2004542c12548b6c2e49
arecatsreal <Miphamster@protonmail.com>
2022-11-18 04:20:19 -0600

    stuff

--- config.def.h
+++ config.def.h
@@ -1,21 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=10" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=20" };
 static const char dmenufont[]       = "UbuntuMono Nerd Font Mono:size=10";
-static const char dracula_bg[]       = "#282a36";
-static const char col_gray2[]       = "#444444";
-static const char dracula_fg[]       = "#f8f8f2";
-static const char col_gray4[]       = "#eeeeee";
-static const char dracula_purp[]        = "#bd93f9";
+
+//static const char dracula_bg[]	= "#282a36";
+//static const char col_gray2[]	 	= "#444444";
+//static const char dracula_fg[]	= "#f8f8f2";
+//static const char col_gray4[]		= "#eeeeee";
+//static const char dracula_purp[]	= "#bd93f9";
+
+// static const char catppuccin_bg[]		= "#1E1E2E";
+// static const char col_gray2[]			= "#575268";
+// static const char catppuccin_fg[]		= "#D9E0EE";
+// static const char col_gray4[]			= "#575268";
+// static const char catppuccin_lavender[]	= "#C9CBFF";
+
+static const char duskfox_bg[]		= "#232136";
+static const char col_gray2[]			= "#575268";
+static const char duskfox_fg[]		= "#e0def4";
+static const char col_gray4[]			= "#575268";
+static const char duskfox_a[]	= "#9ccfd8";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { dracula_fg, dracula_bg, col_gray2 },
-	[SchemeSel]  = { col_gray4, dracula_purp,  dracula_purp  },
+	[SchemeNorm] = { duskfox_fg, duskfox_bg, col_gray2 },
+	[SchemeSel]  = { duskfox_bg, duskfox_a,  duskfox_a  },
 };
 
 /* tagging */
